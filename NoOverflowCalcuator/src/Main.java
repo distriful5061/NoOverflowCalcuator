7fc4a11b621640cdd119812f9a3162f0660fd5db
rm809 <83632267+distriful5061@users.noreply.github.com>
2022-06-27 22:16:37 +0900

    Update Main.java

--- NoOverflowCalcuator/src/Main.java
+++ NoOverflowCalcuator/src/Main.java
@@ -1,26 +1,50 @@
 import java.util.ArrayList;
 import java.util.List;
 
-public class Main {
+class Studio{
+    protected static void print(String content){
+        System.out.print(content);
+    }
+
+    protected static void print(int content){
+        System.out.print(content);
+    }
+
+    protected static boolean MinusCheck(int num){
+        return Math.abs(num) != num;
+    }
+}
+
+public class Main extends Studio{
     public static List<Integer> Numbers = new ArrayList<>();
+    public static boolean isMinus = false;
     private static final int TEN = 10;
+    private static final String N = "\n";
 
     public static void main(String[] args) {
-        Numbers.add(2);
-        print(Numbers.get(0)+"\n");
+        FP();//Do not change it!
+
+        //Main start here
+        add(2);
+        print(getNumber()+" "+getLength()+N);
         add(12);
-        for(int num : Numbers){
-            print(num + " ");
+        print(getNumber()+" "+getLength());
+        print(N);
+        minus(10);
+        print(getNumber()+" "+getLength()+N);
+    }
+
+    private static String getNumber(){
+        StringBuilder string = new StringBuilder();
+        for(int i : Numbers){
+            string.append(i);
         }
-        print("\n");
-    }
 
-    private static void print(String content){
-        System.out.print(content);
-    }
+        if(isMinus){
+            string.append("-");
+        }
 
-    private static void print(int content){
-        System.out.print(content);
+        return string.reverse().toString();
     }
 
     private static int getLength(){
@@ -32,6 +56,7 @@ public class Main {
     }
 
     private static void check(){
+
         int length = getLength();
         for(int i=0;i<length;i++){
             int num1 = Numbers.get(i);
@@ -49,6 +74,11 @@ public class Main {
     }
 
     private static void add(int num){
+        if(MinusCheck(num)){
+            minus(Math.abs(num));
+            return;
+        }
+
         if(Numbers.isEmpty()){
             Numbers.add(0);
         }
@@ -56,5 +86,25 @@ public class Main {
         ZeroNum += num;
         Numbers.set(0,ZeroNum);
         check();
+    }
+
+    private static void minus(int num){
+        if(MinusCheck(num)){
+            add(Math.abs(num));
+            return;
+        }
+
+        if(Numbers.isEmpty()){
+            Numbers.add(0);
+        }
+
+        int ZeroNum = Numbers.get(0);
+        ZeroNum -= num;
+        Numbers.set(0,ZeroNum);
+        check();
+    }
+
+    private static void FP(){
+        Numbers.add(0);
     }
 }
